5149557317beb450e011e7dd47a614fdae16d806
Dmitry <dmitrytbp@gmail.com>
2020-12-29 19:50:46 +0700

    Added controllers

---
+++ controllers/category.js
@@ -0,0 +1,19 @@
+module.exports.getAll = function(res, req) {
+  
+}
+
+module.exports.getById = function(res, req) {
+  
+}
+
+module.exports.delete = function(res, req) {
+  
+}
+
+module.exports.create = function(res, req) {
+  
+}
+
+module.exports.update = function(res, req) {
+  
+}
---
+++ controllers/position.js
@@ -0,0 +1,15 @@
+module.exports.getByCategoryId = function(res, req) {
+  
+}
+
+module.exports.create = function(res, req) {
+  
+}
+
+module.exports.update = function(res, req) {
+  
+}
+
+module.exports.delete = function(res, req) {
+  
+}
