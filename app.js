2363a9ef4edb958c611a3d8f420b03233124de6d
Dmitry <dmitrytbp@gmail.com>
2020-12-30 19:20:04 +0700

    Update app.js
    Added mangoose, cors, morgan. Added connected to database

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mangoose = require('mongoose');
 
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
+const keys = require('./config/keys');
 
 const app = express();
 
+mangoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connected')
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(require('cors')());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
