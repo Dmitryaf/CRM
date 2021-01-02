5e61066a0338797734014ef48406e958a719ec83
Dmitry <dmitrytbp@gmail.com>
2021-01-02 16:22:29 +0700

    init passport

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mangoose = require('mongoose');
+const passport = require('passport');
 
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
@@ -11,16 +12,23 @@ const keys = require('./config/keys');
 
 const app = express();
 
-mangoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('MongoDB connected')
-    })
-    .catch(error => {
-        console.log(error)
-    })
+mangoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+app.use(passport.initialize());
+require('./middleware/passport')(passport);
 
 app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(require('cors')());
 
