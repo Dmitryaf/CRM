c180e7d1627ddb54ef202a1c44909c070eb22e74
Dmitry <dmitrytbp@gmail.com>
2021-01-02 16:49:37 +0700

    Added errorHandler

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 const keys = require('../config/keys');
+const errorHandler = require('../utils/errorHandler');
+
 module.exports.login = async function (req, res) {
   const candidate = await User.findOne({ email: req.body.email });
 
@@ -16,23 +18,23 @@ module.exports.login = async function (req, res) {
       const token = jwt.sign(
         {
           email: candidate.email,
-          userId: candidate._id,
+          userId: candidate._id
         },
         keys.jwt,
         { expiresIn: 60 * 60 }
       );
 
       res.status(200).json({
-        token: `Bearer ${token}`,
+        token: `Bearer ${token}`
       });
     } else {
       res.status(401).json({
-        message: 'Пароли не совпадают.',
+        message: 'Пароли не совпадают.'
       });
     }
   } else {
     res.status(404).json({
-      message: 'Пользователь не найден.',
+      message: 'Пользователь не найден.'
     });
   }
 };
@@ -42,7 +44,7 @@ module.exports.register = async function (req, res) {
 
   if (candidate) {
     res.status(409).json({
-      message: 'Такой email уже существует.',
+      message: 'Такой email уже существует.'
     });
   } else {
     const salt = bcrypt.genSaltSync(10);
@@ -50,14 +52,14 @@ module.exports.register = async function (req, res) {
 
     const user = new User({
       email: req.body.email,
-      password: bcrypt.hashSync(password, salt),
+      password: bcrypt.hashSync(password, salt)
     });
 
     try {
       await user.save();
       res.status(201).json(user);
-    } catch (e) {
-      console.error(e);
+    } catch (error) {
+      errorHandler(res, error);
     }
   }
 };
