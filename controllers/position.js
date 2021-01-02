e4a004ad1a36f6a822299b037aa7afb35c148a05
Dmitry <dmitrytbp@gmail.com>
2021-01-02 16:20:45 +0700

    Update

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,7 @@
-module.exports.getByCategoryId = function(res, req) {
-  
-}
+module.exports.getByCategoryId = function (res, req) {};
 
-module.exports.create = function(res, req) {
-  
-}
+module.exports.create = function (res, req) {};
 
-module.exports.update = function(res, req) {
-  
-}
+module.exports.update = function (res, req) {};
 
-module.exports.delete = function(res, req) {
-  
-}
+module.exports.delete = function (res, req) {};
