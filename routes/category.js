87a5cb51d37e80ece3abef0a89de84a4a2dcf70a
Dmitry <dmitrytbp@gmail.com>
2021-01-02 16:21:59 +0700

    Passport authenticate route

--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const controller = require('../controllers/category');
 const router = express.Router();
+const passport = require('passport');
 
-router.get('/', controller.getAll);
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  controller.getAll
+);
 router.get('/:id', controller.getById);
 router.delete('/:id', controller.delete);
 router.post('/', controller.create);
