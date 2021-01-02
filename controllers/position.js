f089909bf3b6b2ae388b2dda90f9907e747ca604
Dmitry <dmitrytbp@gmail.com>
2021-01-02 18:00:20 +0700

    Added methods of position

--- controllers/position.js
+++ controllers/position.js
@@ -1,7 +1,52 @@
-module.exports.getByCategoryId = function (res, req) {};
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
 
-module.exports.create = function (res, req) {};
+module.exports.getByCategoryId = async function (req, res) {
+  try {
+    const positions = await Position.find({
+      id: req.params.categoryId,
+      user: req.user.id
+    });
+    res.status(200).json({ positions });
+  } catch (error) {
+    errorHandler(res, error);
+  }
+};
 
-module.exports.update = function (res, req) {};
+module.exports.create = async function (req, res) {
+  try {
+    const position = await new Position({
+      name: req.body.name,
+      const: req.body.cost,
+      category: req.body.category,
+      user: req.user.id
+    }).save();
+    res.status(201).json(position);
+  } catch (error) {
+    errorHandler(res, error);
+  }
+};
 
-module.exports.delete = function (res, req) {};
+module.exports.update = async function (req, res) {
+  try {
+    const position = await Position.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(position);
+  } catch (error) {
+    errorHandler(res, error);
+  }
+};
+
+module.exports.delete = async function (req, res) {
+  try {
+    await Position.remove({ _id: req.params.id });
+    res.status(200).json({
+      message: 'Позиция была удаленна'
+    });
+  } catch (error) {
+    errorHandler(res, error);
+  }
+};
