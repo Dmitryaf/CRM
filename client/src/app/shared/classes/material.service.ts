f39feb28dfe5c5605883d1b6ab99a7c1999e27db
Dmitry <dmitrytbp@gmail.com>
2021-01-20 12:53:18 +0500

    Update material.service.ts

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -1,7 +1,12 @@
+import { ElementRef } from '@angular/core';
 declare var M;
 
 export class MaterialService {
   static toast(message: string): void {
     M.toast({ html: message });
+  }
+
+  static initFloatingButton(ref: ElementRef) {
+    M.FloatingActionButton.init(ref.nativeElement);
   }
 }
