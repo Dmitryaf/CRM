d6eab08a06266be8e1f31762ee8a7008902f8ea0
Dmitry <dmitrytbp@gmail.com>
2021-01-21 16:38:55 +0500

    Added create and update methods

--- client/src/app/shared/services/categories.service.ts
+++ client/src/app/shared/services/categories.service.ts
@@ -16,4 +16,26 @@ export class CategoriesService {
   getById(id: string): Observable<Category> {
     return this.http.get<Category>(`/api/categories/${id}`);
   }
+
+  create(name: string, img?: File): Observable<Category> {
+    const formData: FormData = new FormData();
+
+    if (img) {
+      formData.append('image', img, img.name);
+    }
+    formData.append('name', name);
+
+    return this.http.post<Category>('/api/categories/', formData);
+  }
+
+  update(id: string, name: string, img?: File): Observable<Category> {
+    const formData: FormData = new FormData();
+
+    if (img) {
+      formData.append('image', img, img.name);
+    }
+    formData.append('name', name);
+
+    return this.http.patch<Category>(`/api/categories/${id}`, formData);
+  }
 }
