9f26182155a0c20c927e3f21a323a5fc00c61d26
Dmitry <dmitrytbp@gmail.com>
2021-01-21 17:08:12 +0500

    Update interfaces.ts

--- client/src/app/shared/interfaces.ts
+++ client/src/app/shared/interfaces.ts
@@ -1,11 +1,14 @@
 export interface User {
-  email: string,
-  password: string
+  email: string;
+  password: string;
 }
 
 export interface Category {
-  name: string,
-  imgSrc?: string,
-  user?: string,
-  _id?: string
+  name: string;
+  imgSrc?: string;
+  user?: string;
+  _id?: string;
+}
+export interface Message {
+  message: string;
 }
