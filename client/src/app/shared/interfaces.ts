0a9b2e47005a1ebc2ce57a416471999310c678c8
Dmitry <dmitrytbp@gmail.com>
2021-01-22 11:26:20 +0500

    Added position interface

--- client/src/app/shared/interfaces.ts
+++ client/src/app/shared/interfaces.ts
@@ -12,3 +12,11 @@ export interface Category {
 export interface Message {
   message: string;
 }
+
+export interface Position {
+  name: string;
+  cost: number;
+  user?: string;
+  category: string;
+  _id?: string;
+}
