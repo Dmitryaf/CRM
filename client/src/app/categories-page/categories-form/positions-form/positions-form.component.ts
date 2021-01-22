a31d0fc441294c038b08241916fd82b6098e67e1
Dmitry <dmitrytbp@gmail.com>
2021-01-22 11:36:21 +0500

    Update positions-form.component.ts
    Implemented getting positions of category.
    Implemented init material modal

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,13 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { MaterialService, MaterialInstance } from './../../../shared/classes/material.service';
+import { Position } from './../../../shared/interfaces';
+import { PositionService } from './../../../shared/services/positions.service';
+import { Component, Input, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-positions-form',
   templateUrl: './positions-form.component.html',
   styleUrls: ['./positions-form.component.scss']
 })
-export class PositionsFormComponent implements OnInit {
-  constructor() { }
+export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy {
+  @Input('categoryId') categoryId: string;
+  @ViewChild('modal') modalRef: ElementRef;
 
-  ngOnInit(): void { }
+  positions: Position[] = [];
+  loading: boolean = false;
+  modal: MaterialInstance;
+
+  constructor(
+    private positionService: PositionService
+  ) { }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.positionService.fetch(this.categoryId).subscribe((positions) => {
+      this.positions = positions;
+      this.loading = false;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
+
+  ngOnDestroy(): void {
+    this.modal.destroy();
+  }
+
+  onAddPosition(): void {
+    this.modal.open();
+  }
+
+  onCancel(): void {
+    this.modal.close();
+  }
+
+  onSelectPosition(position: Position) {
+    this.modal.open();
+  }
 
 }
