86e2194f1595048b60226e7d5e0bf8278872c96f
Dmitry <dmitrytbp@gmail.com>
2021-01-25 13:53:42 +0500

    Init positions and added addToOrder method

--- client/src/app/order-page/order-positions/order-positions.component.ts
+++ client/src/app/order-page/order-positions/order-positions.component.ts
@@ -1,3 +1,9 @@
+import { OrderService } from './../../shared/services/order.service';
+import { map, switchMap } from 'rxjs/internal/operators';
+import { Position } from './../../shared/interfaces';
+import { Observable } from 'rxjs';
+import { PositionsService } from './../../shared/services/positions.service';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-positions.component.scss']
 })
 export class OrderPositionsComponent implements OnInit {
+  positions$: Observable<Position[]>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private positionsService: PositionsService,
+    private orderService: OrderService
+  ) { }
 
   ngOnInit(): void {
+    this.positions$ = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.positionsService.fetch(params['id']);
+      }),
+      map(
+        (positions: Position[]) => {
+          return positions.map((position) => {
+            position.quantity = 1;
+            return position;
+          })
+        }
+      )
+    );
+  }
+
+  addToOrder(position: Position) {
+    this.orderService.add(position);
   }
 
 }
