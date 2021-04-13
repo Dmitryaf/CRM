98e3c7d65fa2e130a41f85f0f2420e45ada00240
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-13 17:59:45 +0500

    Update

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Order } from './../../shared/interfaces';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-history-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history-list.component.scss']
 })
 export class HistoryListComponent implements OnInit {
+  @Input() orders: Order[];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  computePrice(order: Order): number {
+    return order.list.reduce((total, item) => {
+      return total += item.quantity * item.cost
+    }, 0)
   }
 
 }
--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,16 +1,55 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core'
-import { MaterialInstance, MaterialService } from '../shared/classes/material.service'
+import { Order } from './../shared/interfaces';
+import { Subscription } from 'rxjs';
+import { OrdersService } from './../shared/services/orders.service';
+import { Component, OnInit, OnDestroy } from '@angular/core'
 
+const STEP = 2;
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.scss']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  orders: Order[] = [];
+  oSub: Subscription;
+
+  offset = 0;
+  limit = STEP;
+
+  loading = false;
+  reloading = false;
+  noMoreOrders = false;
+
+  constructor(private ordersService: OrdersService) { }
 
   ngOnInit() {
+    this.reloading = true;
+    this.fetch();
   }
+
+  private fetch() {
+    const params = {
+      offset: this.offset,
+      limit: this.limit
+    };
+    this.oSub = this.ordersService.fetch(params).subscribe(orders => {
+      this.orders = this.orders.concat(orders);
+      this.noMoreOrders = orders.length < STEP;
+      this.loading = false;
+      this.reloading = false;
+    })
+  }
+
+  loadMore() {
+    this.offset += STEP;
+    this.loading = true;
+    this.fetch();
+  }
+
+  ngOnDestroy() {
+    this.oSub.unsubscribe();
+  }
+
 
 }
